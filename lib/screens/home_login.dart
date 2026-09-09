d8f76a030c80142cf21efb1d82413369d5ad6b01
radit-thy <radit.thy24@gmail.com>
2026-09-09 20:25:45 +0700

    fix code ryna

--- lib/screens/home_login.dart
+++ lib/screens/home_login.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'register.dart';
-import 'home_page.dart';
+import 'dashboard.dart';
 
 class HomeLogin extends StatefulWidget {
   const HomeLogin({super.key});
@@ -69,7 +69,7 @@ class _HomeLoginState extends State<HomeLogin> {
 
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
+        MaterialPageRoute(builder: (context) => const DashboardPage()),
       );
     } on FirebaseAuthException catch (e) {
       if (!mounted) return;
