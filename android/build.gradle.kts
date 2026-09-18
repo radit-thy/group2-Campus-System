5c7822d219e92ec84c7320337edf2bf775ed5210
radit-thy <radit.thy24@gmail.com>
2026-09-18 19:52:16 +0700

    push apk

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,8 @@
-plugins {
-  // ...
-  // Add the dependency for the Google services Gradle plugin
-  id("com.google.gms.google-services") version "4.5.0" apply false
-}
+// plugins {
+//   // ...
+//   // Add the dependency for the Google services Gradle plugin
+// //   id("com.google.gms.google-services") version "4.5.0" apply false
+// }
 
 allprojects {
     repositories {
